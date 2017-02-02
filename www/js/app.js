0f9cefd9894cd7234afc937ba03ca8b6eeea4a7f
Daniel Olita <danielolita1@gmail.com>
2017-02-02 12:15:48 -0600

    Added routes, templates, and starter controllers for different pages.

--- www/js/app.js
+++ www/js/app.js
@@ -6,31 +6,60 @@
 var app = angular.module('masa', ['ionic', 'ionic-material']);
 
 app.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
 
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-    });
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
 })
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
+  $stateProvider
 
-    .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'AppCtrl'
-    })
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
 
-    ;
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/components');
+  .state('app.plan', {
+    url: '/plan',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/plan.html',
+        controller: 'PlanCtrl'
+      }
+    }
+  })
+
+  .state('app.history', {
+    url: '/history',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/history.html',
+        controller: 'HistCtrl'
+      }
+    }
+  })
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
 });
