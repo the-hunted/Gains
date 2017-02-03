77aafb684532bc6f3d6a9df48c642ccfc3c802d8
Daniel Olita <danielolita1@gmail.com>
2017-02-02 20:20:43 -0600

    Added add, get and remove for localStorage. Added ngStorage. Got data for history in template. Working on making it look good.

--- www/js/controllers/HistCtrl.js
+++ www/js/controllers/HistCtrl.js
@@ -1,3 +1,12 @@
-app.controller('HistCtrl', function ($scope) {
+app.controller('HistCtrl', function ($scope, WorkoutsFac, StorageFac) {
+  $scope.history = WorkoutsFac.getWorkoutHistory()
 
+  if ($scope.history) {
+    console.log($scope.history);
+    $scope.dates = $scope.history.map(function(exerciseObj) {
+      return Object.keys(exerciseObj)[0];
+    });
+
+    console.log($scope.dates);
+  }
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,46 +1,120 @@
-app.factory('StorageFac', function ($http, $window, $localStorage) { // throw into workout planner controller
+angular.module('masa.services', ['ngStorage'])
+.factory('StorageFac', function($localStorage) {
+  $localStorage = $localStorage.$default({
+    workouts: {}
+  });
 
-  var _add = function(exerciseData) {
-    var date = new Date().setHours(0, 0, 0, 0);
-    if(!$localStorage.workouts){
-      $localStorage.workouts = {};
-    }
+  var getAll = function() {
+    return $localStorage.workouts;
+  };
+
+  var remove = function(date) {
+    delete $localStorage.workouts[date];
+  };
+
+  var add = function(date, exerciseData) {
     $localStorage.workouts[date] = exerciseData;
-    console.log('$localStorage', $localStorage);
   };
 
   return {
-    _add: _add
+    getAll: getAll,
+    remove: remove,
+    add: add
   };
 })
 
-.factory('WorkoutFac', function ($http, $window, $localStorage) { // throw into workout planner controller
-
+.factory('WorkoutsFac', function ($http, $window, StorageFac) { // throw into workout planner controller
   var storeWorkout = function(exercisesData) {
-    return $http({
-      method: 'POST',
-      url: '/workoutHistory',
-      data: exercisesData
-    })
-    .then(function(res) {
-      console.log('Successfully posted the data server side where it can be stored in the user\'s db:', res.data);
-    }, function(err) {
-      console.error(err);
-    })
+    StorageFac.add(exercisesData.date, exercisesData.data);
   };
 
   var getWorkoutHistory = function() {
-    return $http({
-      method: 'GET', // changed from GET becaus
-      url: '/workoutHistory/' + $window.localStorage.getItem('user')
-    })
-    .then(function(res) {
-      console.log('Successfully retrieved user\'s workout history from the db:', res.data);
-      return res.data;
-    }, function(err) {
-      console.error(err);
-      return err;
-    })
+    var workouts = StorageFac.getAll();
+    if (!workouts) {
+      return false;
+    }
+    var MILI_DAY = 86400000;
+
+    var today = (new Date()).setHours(0, 0, 0, 0);
+    var sortedDates = Object.keys(workouts).sort();
+
+    if (today in workouts) {
+      sortedDates.splice(0, sortedDates.indexOf(today) + 1);
+    } else {
+      if (sortedDates[0] > today) {
+        return false;
+      }
+      var foundClosestDate = false;
+      var dateToCheck = today - MILI_DAY;
+
+      while (!foundClosestDate) {
+        if (dateToCheck in workouts) {
+          sortedDates.splice(0, sortedDates.indexOf(dateToCheck) + 1);
+          foundClosestDate = true;
+        } else {
+          dateToCheck -= MILI_DAY;
+        }
+      }
+    }
+
+    var workoutsToShow = [];
+
+    var currDate = today;
+
+    for (let i = sortedDates.length - 1; i >= sortedDates.length - 11 && i >= 0; i--) {
+      if (sortedDates[i] in workouts) {
+        var exercise = {};
+        exercise[currDate] = workouts[currDate];
+        workoutsToShow.push(exercise);
+      }
+      currDate -= MILI_DAY;
+    }
+
+    return workoutsToShow;
+  };
+
+  // var getWorkoutHistory = function() {
+  //   return $http({
+  //     method: 'GET', // changed from GET becaus
+  //     url: '/workoutHistory/' + $window.localStorage.getItem('user')
+  //   })
+  //   .then(function(res) {
+  //     console.log('Successfully retrieved user\'s workout history from the db:', res.data);
+  //     return res.data;
+  //   }, function(err) {
+  //     console.error(err);
+  //     return err;
+  //   })
+  // };
+  //
+  // var storeWorkout = function(exercisesData) {
+  //   return $http({
+  //     method: 'POST',
+  //     url: '/workoutHistory',
+  //     data: exercisesData
+  //   })
+  //   .then(function(res) {
+  //     console.log('Successfully posted the data server side where it can be stored in the user\'s db:', res.data);
+  //   }, function(err) {
+  //     console.error(err);
+  //   })
+  // };
+
+  var decode = function() {
+    console.log('decoding user token');
+    var token = $window.localStorage.getItem('masaToken');
+    console.log('token:', token);
+    var user;
+
+    if (!token) {
+      console.log('the user has no TOKEN!');
+    }
+
+    else {
+      user = jwt.decode(token, 'secret');
+      return user;
+    }
+
   };
 
   return {
@@ -93,4 +167,3 @@ app.factory('StorageFac', function ($http, $window, $localStorage) { // throw in
     }
 
   });
-  
