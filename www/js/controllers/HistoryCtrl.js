d506e52e68d50a3d7acfec23b49aff2237f29ea2
Daniel Olita <danielolita1@gmail.com>
2017-02-04 16:28:03 -0600

    Added delete capability to history page. Added some css.

--- www/js/controllers/HistoryCtrl.js
+++ www/js/controllers/HistoryCtrl.js
@@ -1,14 +1,20 @@
 app.controller('HistCtrl', function ($scope, WorkoutsFac, StorageFac, LokiFac) {
+  LokiFac.initDB();
+  $scope.history = [];
+
   $scope.toggleGroup = function(group) {
     group.show = !group.show;
   };
   $scope.isGroupShown = function(group) {
     return group.show;
   };
-  
-  $scope.history;
+
+  $scope.onItemDelete = function(item) {
+    $scope.history.splice($scope.history.indexOf(item), 1);
+    LokiFac.deleteWorkout(item.date);
+  };
+
   $scope.$watch('$viewContentLoaded', function(){
-    LokiFac.initDB();
     LokiFac.getAll()
       .then(function(wods){
         $scope.history = WorkoutsFac.getWorkoutHistory();
