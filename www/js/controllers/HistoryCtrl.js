cd8b2a60ffa59879834dfd5cfe848eeebeb93ab8
Tai Hovanky <tai.hovanky@gmail.com>
2017-02-04 13:30:21 -0600

    updates

--- www/js/controllers/HistoryCtrl.js
+++ www/js/controllers/HistoryCtrl.js
@@ -1,4 +1,11 @@
 app.controller('HistCtrl', function ($scope, WorkoutsFac, StorageFac, LokiFac) {
+  $scope.toggleGroup = function(group) {
+    group.show = !group.show;
+  };
+  $scope.isGroupShown = function(group) {
+    return group.show;
+  };
+  
   $scope.history;
   $scope.$watch('$viewContentLoaded', function(){
     LokiFac.initDB();
